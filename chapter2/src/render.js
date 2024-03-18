dedda994e988d55789fc77af25fe777cd10f5eae
ChyunKim <co1995@naver.com>
2024-03-18 20:30:35 +0900

    add: 과제 2주차 세팅

---
+++ chapter2/src/render.js
@@ -0,0 +1,49 @@
+// virtual Dom을 구성
+// React.createElement 라는 React 내부의 자체 메서드를 통해서 js 객체로 변환됨
+export function jsx(type, props, ...children) {
+  return {
+    type,
+    props,
+    children: [...children],
+  };
+}
+// Babel은 JSX를 React.createElement() 호출로 컴파일
+// react를 임포트 해야하는 이유도 이와 같음
+
+export function createElement(node) {
+  const element = document.createElement(node.type);
+
+  return element;
+}
+
+function updateAttributes(target, newProps, oldProps) {
+  // newProps들을 반복하여 각 속성과 값을 확인
+  //   만약 oldProps에 같은 속성이 있고 값이 동일하다면
+  //     다음 속성으로 넘어감 (변경 불필요)
+  //   만약 위 조건에 해당하지 않는다면 (속성값이 다르거나 구속성에 없음)
+  //     target에 해당 속성을 새 값으로 설정
+  // oldProps을 반복하여 각 속성 확인
+  //   만약 newProps들에 해당 속성이 존재한다면
+  //     다음 속성으로 넘어감 (속성 유지 필요)
+  //   만약 newProps들에 해당 속성이 존재하지 않는다면
+  //     target에서 해당 속성을 제거
+}
+
+export function render(parent, newNode, oldNode, index = 0) {
+  // 1. 만약 newNode가 없고 oldNode만 있다면
+  //   parent에서 oldNode를 제거
+  //   종료
+  // 2. 만약 newNode가 있고 oldNode가 없다면
+  //   newNode를 생성하여 parent에 추가
+  //   종료
+  // 3. 만약 newNode와 oldNode 둘 다 문자열이고 서로 다르다면
+  //   oldNode를 newNode로 교체
+  //   종료
+  // 4. 만약 newNode와 oldNode의 타입이 다르다면
+  //   oldNode를 newNode로 교체
+  //   종료
+  // 5. newNode와 oldNode에 대해 updateAttributes 실행
+  // 6. newNode와 oldNode 자식노드들 중 더 긴 길이를 가진 것을 기준으로 반복
+  //   각 자식노드에 대해 재귀적으로 render 함수 호출
+  parent.appendChild(createElement(newNode));
+}
