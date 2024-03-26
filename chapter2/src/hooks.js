b7cf871d4dea5683a9494f34d1bc951c344213a2
ChyunKim <co1995@naver.com>
2024-03-26 19:49:57 +0900

    add: useState, useMemo 구현

--- chapter2/src/hooks.js
+++ chapter2/src/hooks.js
@@ -1,31 +1,57 @@
 export function createHooks(callback) {
-  // 클로저를 통해 해결
-  // 클래스형 컴포넌트를 render() 메서드를 통해 상태 변경을 감지할 수 있지만 함수형 컴포넌트는 렌더링이 발생하면 함수 자체가 다시 호출됨
-  // 상태를 관리하려면 함수가 다시 호출되었을때 이전 상태 기억해야함
-  // 이문제를 클로저를 통해 해결
-  // 클로저는 내부 함수에서 상위 함수 스코프의 변수에 접근할 수 있는 개념
-  const hooks = [];
-  let index = 0;
+  const stateContext = {
+    current: 0,
+    states: [],
+  };
+
+  const memoContext = {
+    current: 0,
+    memos: [],
+  };
+
+  function resetContext() {
+    stateContext.current = 0;
+    memoContext.current = 0;
+  }
 
   const useState = (initState) => {
-    const state = hooks[index] || initState;
-    hooks[index] = state;
-    const currentIndex = index;
-    const setState = (value) => {
-      hooks[currentIndex] = value;
+    const { current, states } = stateContext;
+    stateContext.current += 1;
+
+    states[current] = states[current] ?? initState;
+
+    const setState = (newState) => {
+      if (newState === states[current]) return;
+      states[current] = newState;
       callback();
     };
-    index++;
 
-    return [state, setState];
+    return [states[current], setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
-  };
+    const { current, memos } = memoContext;
+    memoContext.current += 1;
 
-  const resetContext = () => {
-    index = 0;
+    const memo = memos[current];
+
+    const resetAndReturn = () => {
+      const value = fn();
+      memos[current] = {
+        value,
+        refs,
+      };
+      return value;
+    };
+
+    if (!memo) {
+      return resetAndReturn();
+    }
+
+    if (refs.length > 0 && memo.refs.find((v, k) => v !== refs[k])) {
+      return resetAndReturn();
+    }
+    return memo.value;
   };
 
   return { useState, useMemo, resetContext };
